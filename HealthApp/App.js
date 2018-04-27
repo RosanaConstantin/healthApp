4b19b485050ba1a1c8c4d096b22692a913e4b55c
RosanaConstantin <rosana.constantin@ab4.systems>
2018-04-27 14:21:09 +0300

    added first page design

--- HealthApp/App.js
+++ HealthApp/App.js
@@ -1,13 +1,41 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, TouchableOpacity, Image, Button, Alert } from 'react-native';
+import { Constants } from 'expo';
 
-export default class App extends React.Component {
+import "@expo/vector-icons"; // 6.3.1
+
+export default class App extends Component {
+  _handleButtonPress = () => {
+    Alert.alert(
+      'Button pressed!',
+      'You did it!',
+    );
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text style={styles.title}> 
+        HealthApp
+        </Text>
+         <Text style={styles.paragraph}> 
+        Keep in touch with your health
+        </Text>
+        <Image
+          source={require('./assets/pulse.png')}
+          style={{ height: 140, width: 360 }}
+        />
+      
+         <TouchableOpacity style={styles.singIn}
+          title="Sing in"
+          onPress={this._handleButtonPress}>
+          </TouchableOpacity>
+      
+        <Button style={styles.joinIn}
+          title="Join in"
+          onPress={this._handleButtonPress}
+        />
+      
       </View>
     );
   }
@@ -16,8 +44,30 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#56C4BE',
   },
+  title: {
+    fontSize: 50,
+    fontWeight: '500',
+    textAlign: 'center',
+    color: '#FFFFFF',
+  },
+  paragraph: {
+    fontWeight: '300',
+    color: '#FFFFFF',
+    fontSize: 15,
+    textAlign: 'center'
+  },
+  singIn: {
+    backgroundColor: '#47525E',
+    width: 100,
+    margin: 100,
+    padding: 10,
+    color: '#fffff'
+  //  textAlign: 'center'
+    
+  }
 });
