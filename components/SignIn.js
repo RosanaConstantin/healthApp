68aa3f8607926d754f9f7f150a290af6882e13bd
constantin.rosana@yahoo.com <constantin.rosana@yahoo.com>
2018-05-02 00:30:56 +0300

    added signIn component content

--- components/SignIn.js
+++ components/SignIn.js
@@ -5,8 +5,23 @@ import { Text, View, StyleSheet, Image, Button, Alert } from 'react-native';
 export default class SignIn extends React.Component {
     render() {
         return (
-          <Text >asadss
-          </Text>
+            <View style={{ paddingVertical: 20 }}>
+            <Card>
+              <FormLabel>Username</FormLabel>
+              <FormInput placeholder="Username..." />
+              <FormLabel>Password</FormLabel>
+              <FormInput secureTextEntry placeholder="Password..." />
+        
+              <Button
+                buttonStyle={{ marginTop: 20 }}
+                backgroundColor="#03A9F4"
+                title="SIGN IN"
+                onPress={() => {
+                  onSignIn().then(() => navigation.navigate("SignedIn")); // NEW LOGIC
+                }}
+              />
+            </Card>
+          </View>
           )
       }
   }
