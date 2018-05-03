c90c38963937ae8dc67fe602fe75afbe60f86eca
constantin.rosana@yahoo.com <constantin.rosana@yahoo.com>
2018-05-04 00:44:51 +0300

    added tabbar on dashboard component

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,123 +1,63 @@
 import React, { Component } from 'react';
 import { Text, View, StyleSheet, Image, Button, Alert } from 'react-native';
-import { Constants } from 'expo';
-import { StackNavigator } from 'react-navigation'; // Version can be specified in package.json
-import SignLogic from './SignIn';
-import JoinIn from './JoinIn';
+import Tabbar from 'react-native-tabbar-bottom'
 
-class SignLogicScreen extends React.Component {
-    render() {
-      return (
-        <SignLogic/>
-      );
+export default class Dashboard extends Component {
+  constructor() {
+    super()
+    this.state = {
+      page: "HomeScreen",
     }
   }
-  
-  class JoinInScreen extends React.Component {
-    render() {
-      return (
-        <JoinIn/>
-      );
-    }
-  }
-  
-  // The stylesheet is here, and then below it I have:
- class InitialPageScreen extends React.Component {
-    render() {
-        return (
-            <View style={styles.container}>
-      
-              <Text style={styles.title}>
-                HealthApp
-              </Text>
-      
-              <Text style={styles.paragraph}>
-                Keep in touch with your health
-              </Text>
-      
-              <Image
-                source={require('../assets/pulse.png')}
-                style={{ height: 140, width: 410 }}
-              />
-      
-              <View style={{ marginTop: 50, height: 50, width: 100 }} >
-                <Button style={styles.singIn}
-                  title="Sign in"
-                  color="#841584"
-                  onPress={() => this.props.navigation.navigate('SignLogic')}
-                />
-              </View>
-      
-              <View style={{ marginTop: 20, width: 100 }} >
-                <Button style={styles.joinIn}
-                  title="Join in"
-                  color="#841584"
-                  onPress={() => this.props.navigation.navigate('JoinIn')}
-                />
-              </View>
-            </View>
-            
-          );
-      }
-  }
-  
-  const RootStack = StackNavigator(
-    {
-      InitialPage: {
-        screen: InitialPageScreen,
-      },
-      SignLogic: {
-        screen: SignLogicScreen,
-      }, 
-       JoinIn: {
-        screen: JoinInScreen,
-      },
-    },
-    {
-      initialRouteName: 'InitialPage',
-    }
-  );
 
-  export default class Stack extends React.Component {
-    render() {
-    return (<RootStack style={styles.root}/>);
-    }
+  render() {
+    return (
+      <View style={styles.container}>
+        
+        {this.state.page === "HomeScreen"  && <Text>Screen1</Text>}
+        {this.state.page === "NotificationScreen" && <Text>Screen2</Text>}
+        {this.state.page === "ProfileScreen" && <Text>Screen3</Text>}
+        {this.state.page === "ChatScreen" && <Text>Screen4</Text>}
+        {this.state.page === "SearchScreen" && <Text>Screen5</Text>}
+
+        <Tabbar
+          stateFunc={(tab) => {
+            this.setState({page: tab.page})
+          }}
+          activePage={this.state.page}
+
+          tabs={[
+            {
+              page: "HomeScreen",
+              icon: "home",
+            },
+            {
+              page: "NotificationScreen",
+              icon: "notifications",
+              badgeNumber: 11,
+            },
+            {
+              page: "ProfileScreen",
+              icon: "person",
+            },
+            {
+              page: "ChatScreen",
+              icon: "chatbubbles",
+              badgeNumber: 7,
+            },
+            {
+              page: "SearchScreen",
+              icon: "search",
+            }
+          ]}
+        />
+      </View>
+    );
   }
+}
 
 const styles = StyleSheet.create({
-    root: {
-      backgroundColor: '#56C4BE'
-    },
-    container: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      paddingTop: Constants.statusBarHeight,
-      backgroundColor: '#56C4BE',
-    },
-    title: {
-      fontSize: 50,
-      fontWeight: '500',
-      textAlign: 'center',
-      color: '#FFFFFF',
-    },
-    paragraph: {
-      fontWeight: '300',
-      color: '#FFFFFF',
-      fontSize: 13,
-      marginTop: 20,
-      textAlign: 'center'
-    },
-    singIn: {
-      backgroundColor: '#47525E',
-      width: 100,
-      margin: 100,
-      padding: 10
-    },
-    joinIn: {
-      backgroundColor: '#47525E',
-      width: 100,
-      margin: 100,
-      padding: 10
-    }
-  });
+  container: {
+    flex: 1
+  }
+});
